2adb394bc78e7f8540ac7eae95db6185e4872346
Biswarup35 <biswarup35@gmail.com>
2022-05-13 11:51:03 +0530

    feat(chips): add ability to change size
    Adding ability to change size through props: default is set to "medium"

--- packages/ui/Chips.tsx
+++ packages/ui/Chips.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 export const Chips: React.FC<{
   shape?: "rounded" | "square";
+  size?: "small" | "medium" | "large";
 }> = (props) => {
   return <button {...props} />;
 };
