5548dd4b70d437d43e1fc01aef236334a74d1e60
Biswarup Bouri <biswarup35@gmail.com>
2022-05-14 12:39:24 +0530

    feat(chips): add icon support
    Added trailingIcon support

--- packages/ui/Chips.tsx
+++ packages/ui/Chips.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 export const Chips: React.FC<{
   shape?: "rounded" | "square";
   size?: "small" | "medium" | "large";
+  trailingIcon?: React.ReactNode;
 }> = (props) => {
   return <button {...props} />;
 };
