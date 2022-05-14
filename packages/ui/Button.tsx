47f52a916dc79ac06f2c0e2406651b4bcbf19067
Biswarup Bouri <biswarup35@gmail.com>
2022-05-14 12:58:12 +0530

    feat(button): add color prop
    Extending button ability to change color using props.

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 export const Button: React.FC<{
   variant?: "filled" | "outlined" | "elevated";
   size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary" | "default";
 }> = (props) => {
   return <button {...props} />;
 };
