b652026567dfcd8dee4d9d602358d0a1badf3cf5
Biswarup Bouri <biswarup35@gmail.com>
2022-05-14 14:07:39 +0530

    feat(chips): add color prop
    Added ability to change color through props

--- packages/ui/Chips.tsx
+++ packages/ui/Chips.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 export const Chips: React.FC<{
   shape?: "rounded" | "square";
   size?: "small" | "medium" | "large";
+  color?: "primary" | "secondary" | "success" | "warning" | "danger" | "light" | "dark";
   trailingIcon?: React.ReactNode;
 }> = (props) => {
   return <button {...props} />;
