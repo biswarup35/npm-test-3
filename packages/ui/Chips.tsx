ccd76276918cb0abd79a2d95a0dfde91343dad7a
Biswarup Bouri <biswarup35@gmail.com>
2022-05-14 15:41:29 +0530

    feat(chips): add default props

--- packages/ui/Chips.tsx
+++ packages/ui/Chips.tsx
@@ -2,8 +2,21 @@ import * as React from "react";
 export const Chips: React.FC<{
   shape?: "rounded" | "square";
   size?: "small" | "medium" | "large";
-  color?: "primary" | "secondary" | "success" | "warning" | "danger" | "light" | "dark";
+  color?:
+    | "primary"
+    | "secondary"
+    | "success"
+    | "warning"
+    | "danger"
+    | "light"
+    | "dark";
   trailingIcon?: React.ReactNode;
 }> = (props) => {
   return <button {...props} />;
+};
+
+Chips.defaultProps = {
+  shape: "rounded",
+  size: "medium",
+  color: "primary",
 };
